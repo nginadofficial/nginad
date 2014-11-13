4ea0d2b7cd6f68eaad27fad83833f42e9c2a570d
Christopher Gu <cgu1@cdnpal.com>
2014-11-13 10:08:28 -0800

    NGIN-12: Break video and display workflows down into components and also the RTB parsers

---
+++ upload/module/BuyGenericRTB/src/BuyGenericRTB/common/workflows/DisplayWorkflow.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * CDNPAL NGINAD Project
+ *
+ * @link http://www.nginad.com
+ * @copyright Copyright (c) 2013-2015 CDNPAL Ltd. All Rights Reserved
+ * @license GPLv3
+ */
+
+namespace buyrtb\workflows;
+
+abstract class DisplayWorkflow
+{
+	
+	protected $rtb_provider = "none";
+	public static $rtb_child_class_name = "none";
+
+	// singleton
+	private static $_instance;	
+
+	public static function get_instance() {
+		if (self::$_instance == null):
+			self::$_instance = new self::$rtb_child_class_name();
+		endif;
+		return self::$_instance;	
+	}
+	
+    public function process_business_rules_workflow($RtbBid) {
+
+    }
+
+
+}
