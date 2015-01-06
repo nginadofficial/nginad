b973a0444665d540990dc48691afa1495d145c7d
Christopher Gu <cgu1@cdnpal.com>
2015-01-06 08:34:24 -0800

    Fixed fold position check from the banner delivery filter to match the OpenRTB 2.2 specification integers.

--- upload/module/BuyGenericRTB/src/BuyGenericRTB/common/workflows/tasklets/display/adcampaignbannerrestrictions/CheckFoldPosition.php
+++ upload/module/BuyGenericRTB/src/BuyGenericRTB/common/workflows/tasklets/display/adcampaignbannerrestrictions/CheckFoldPosition.php
@@ -15,14 +15,26 @@ class CheckFoldPosition {
 		
 		$RtbBidRequestBanner = $RtbBidRequestImp->RtbBidRequestBanner;
 		
+		$openrtb_fold_pos = 0;
+		
+		if($AdCampaignBannerRestrictions->FoldPos == 1):
+			// above = 1 OpenRTB 2.2
+			$openrtb_fold_pos = 1;
+		elseif($AdCampaignBannerRestrictions->FoldPos == 2):
+			// below = 3 OpenRTB 2.2
+			$openrtb_fold_pos = 3;
+		endif;
+		
 		/*
 		 * Check banner system fold position (sFoldPos), I don't think we can trust the user fold position (uFoldPos)
+		 * Partial above the fold not supported in OpenRTB
 		*/
 		if ($AdCampaignBannerRestrictions->FoldPos !== null && $RtbBidRequestBanner->pos !== null 
-			&& $AdCampaignBannerRestrictions->FoldPos != $RtbBidRequestBanner->pos):
+			&& $AdCampaignBannerRestrictions->FoldPos != 3
+			&& $openrtb_fold_pos != $RtbBidRequestBanner->pos):
 			if ($Logger->setting_log === true):
 				$Logger->log[] = "Failed: " . "Check banner system fold position :: EXPECTED: " 
-					. $AdCampaignBannerRestrictions->FoldPos
+					. $openrtb_fold_pos
 					. " GOT: " . $RtbBidRequestBanner->pos;
 			endif;
 			return false;
