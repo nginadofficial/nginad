061cb87cbb637bea6732828f3cbb7441f141f0fc
Christopher Gu <cgu1@cdnpal.com>
2015-03-22 22:48:55 -0700

    updated monthly invoicing SQL

--- dbschema/util/invoicing.sql
+++ dbschema/util/invoicing.sql
@@ -4,6 +4,7 @@
 
 select 
 au.user_login AS `UserName`,
+au.user_email AS `Email`,
 bshiccs.`AdCampaignBannerID` AS `AdCampaignBannerID`,
 acb.`Name` AS `Banner Name`,
 CONCAT('$', ROUND(sum(bshiccs.`CurrentSpendGross`), 2)) AS `TotalSpendGross`,
@@ -11,7 +12,7 @@ FORMAT(sum(bshiccs.`ImpressionsCounter`), 0) AS `TotalImpressions`
 from `BuySideHourlyImpressionsCounterCurrentSpend` bshiccs
 inner join AdCampaignBanner acb on acb.`AdCampaignBannerID` = bshiccs.`AdCampaignBannerID`
 inner join auth_Users au on acb.UserID = au.user_id
-where bshiccs.DateCreated >= '2015-01-01 00:00:00'
-and bshiccs.DateCreated < '2015-02-01 00:00:00'
-and LOWER(au.user_login) = 'user_login'
-group by bshiccs.`AdCampaignBannerID`
+where bshiccs.DateCreated >= '2015-02-01 00:00:00'
+and bshiccs.DateCreated < '2015-03-01 00:00:00'
+-- and LOWER(au.user_login) = 'username'
+group by LOWER(au.user_login)
