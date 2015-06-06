512bafe39f53358f34b432f0e8e8d10d0410cb06
chrisgu <support@nginad.com>
2015-06-06 11:37:39 -0700

    Turn Fidelity debugging off

--- upload/module/BuySidePartners/FidelityBuysidePartner/src/FidelityBuysidePartner/fidelity/FidelityBuysidePartnerLogger.php
+++ upload/module/BuySidePartners/FidelityBuysidePartner/src/FidelityBuysidePartner/fidelity/FidelityBuysidePartnerLogger.php
@@ -14,10 +14,10 @@ class FidelityBuysidePartnerLogger extends \rtbbuyfidelity\RtbBuyFidelityLogger
 	protected $rtb_provider = "FidelityBuysidePartner";
 	
 	// logging settings
-	public $setting_debug 					= true;
-	public $setting_log 					= true;
-	public $setting_min_log 				= false;
-	public $setting_only_log_bids 			= false;
+	public $setting_debug 					= false;
+	public $setting_log 					= false;
+	public $setting_min_log 				= true;
+	public $setting_only_log_bids 			= true;
 	public $setting_log_to_screen 			= false;
 	public $setting_log_file_location 		= "logs/fidelitybuysidepartner/";
 
