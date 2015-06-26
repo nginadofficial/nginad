beaf458c94ef742cab2fb3e8b84bebadcd7bf0ee
chrisgu <support@nginad.com>
2015-06-26 15:42:03 -0700

    temporarily enabling debugging flags

--- upload/public/index.php
+++ upload/public/index.php
@@ -11,7 +11,8 @@
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
  */
-
+error_reporting(E_ALL);
+ini_set('display_errors', true);
 chdir(dirname(__DIR__));
 
 // Setup autoloading
