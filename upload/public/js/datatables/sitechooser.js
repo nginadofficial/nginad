97d2d73710d6c860804dda2058a9be564fa8f82b
chrisgu <support@nginad.com>
2015-06-30 14:09:05 -0700

    Here is the initial check-in on the datatables.net code.

---
+++ upload/public/js/datatables/sitechooser.js
@@ -0,0 +1,39 @@
+var datatable = null;
+
+// Invocation code  (Publisher/website/zone)
+function ShowChooserSsp () {
+
+	$('#myModalLabel').html("SSP RTB Publisher Inventory Feeds");
+	
+	$('#InvocationCodeModal').modal('show');
+	
+	if (datatable === null) {
+		datatable = $('#example').dataTable( {
+			// "lengthMenu": [[5, 10, 25, 50, -1], [5, 10, 25, 50, "All"]],
+			"ajax": "/data/objects.txt",
+			"columns": [
+				{ "data": " " },
+				{ "data": "Site ID" },
+				{ "data": "Domain Name" },
+				{ "data": "Daily Impressions" },
+				{ "data": "Average CPM" },
+				{ "data": "Exchange" }
+			]
+		} );
+	} else {
+		$('#example').dataTable().api().clear();
+		$('#example').dataTable().api().ajax.url( '/data/objects.txt' ).load();
+	}
+	
+	
+	
+	
+	/*
+	$.post("/publisher/zone/" + domain_id + "/generatetag", { ad_id: ad_id }, function( data ) {
+		$('#adtag_progress_bar').css("display","none");
+		$('#adtag').html(data.data.tag);
+		$('#adtag').css("display","block");
+	},'json');
+	*/
+
+}
