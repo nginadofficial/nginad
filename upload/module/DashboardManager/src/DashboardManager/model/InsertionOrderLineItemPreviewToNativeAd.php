7889b4c8c8e481280aec02b08004e43f03e73084
Christopher Gu <cgu1@nginad.com>
2016-03-23 11:48:08 -0700

    Adding model and DAO classes to link the IO Line items to the native ads from the native ad library.

---
+++ upload/module/DashboardManager/src/DashboardManager/model/InsertionOrderLineItemPreviewToNativeAd.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * NGINAD Project
+ *
+ * @link http://www.nginad.com
+ * @copyright Copyright (c) 2013-2016 NginAd Foundation. All Rights Reserved
+ * @license GPLv3
+ */
+
+namespace model;
+
+class InsertionOrderLineItemPreviewToNativeAd {
+
+	public $InsertionOrderLineItemPreviewToNativeAdID;
+	public $InsertionOrderLineItemPreviewID;
+	public $NativeAdID;
+	public $DateCreated;
+	
+}
+	
---
+++ upload/module/DashboardManager/src/DashboardManager/model/InsertionOrderLineItemToNativeAd.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * NGINAD Project
+ *
+ * @link http://www.nginad.com
+ * @copyright Copyright (c) 2013-2016 NginAd Foundation. All Rights Reserved
+ * @license GPLv3
+ */
+
+namespace model;
+
+class InsertionOrderLineItemToNativeAd {
+
+    public $InsertionOrderLineItemToNativeAdID;
+    public $InsertionOrderLineItemID;
+    public $NativeAdID;
+    public $DateCreated;
+
+}
