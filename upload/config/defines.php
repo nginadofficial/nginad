ac388dfb0dce7dc057e4453df8bf4f4e64aa84b7
Christopher Gu <cgu1@nginad.com>
2016-03-26 19:38:42 -0700

    Add header to the new defines file

--- upload/config/defines.php
+++ upload/config/defines.php
@@ -1,4 +1,11 @@
-<?php 
+<?php
+/**
+ * NGINAD Project
+ *
+ * @link http://www.nginad.com
+ * @copyright Copyright (c) 2013-2016 NginAd Foundation. All Rights Reserved
+ * @license GPLv3
+ */
 
 /*
  * Configure Test User
